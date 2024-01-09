b384740ed9d69ae08171ac637bb4c73ccb70b8bb
caiman48 <caiman48clm@gmail.com>
2024-01-08 19:54:44 -0500

    created eventlistener

---
+++ Assets/script.js/script.js
@@ -0,0 +1,19 @@
+var genresButton = document.getElementById("genresButton");
+        var popularityButton = document.getElementById("popularityButton");
+        var ratingsButton = document.getElementById("ratingsButton");
+
+        
+        genresButton.addEventListener("click", function() {
+            
+            console.log("Genres button clicked");
+        });
+
+        popularityButton.addEventListener("click", function() {
+           
+            console.log("Popularity button clicked");
+        });
+
+        ratingsButton.addEventListener("click", function() {
+           
+            console.log("Ratings button clicked");
+        });
