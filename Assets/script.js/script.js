3ce955e4aad95ef75f2dd35ab826818587c6c466
caiman48 <caiman48clm@gmail.com>
2024-01-10 21:21:24 -0500

    added script code

--- Assets/script.js/script.js
+++ Assets/script.js/script.js
@@ -1,25 +1,44 @@
-//  EventListener 
-var genresButton = document.getElementById("genresButton");
-        var popularityButton = document.getElementById("popularityButton");
-        var ratingsButton = document.getElementById("ratingsButton");
+
+// var genresButton = document.getElementById("genresButton");
+//         var popularityButton = document.getElementById("popularityButton");
+//         var ratingsButton = document.getElementById("ratingsButton");
 
         
-        genresButton.addEventListener("click", function() {
+//         genresButton.addEventListener("click", function() {
             
-            console.log("Genres button clicked");
-        });
+//             console.log("Genres button clicked");
+//         });
 
-        popularityButton.addEventListener("click", function() {
+//         popularityButton.addEventListener("click", function() {
            
-            console.log("Popularity button clicked");
-        });
+//             console.log("Popularity button clicked");
+//         });
 
-        ratingsButton.addEventListener("click", function() {
+//         ratingsButton.addEventListener("click", function() {
            
-            console.log("Ratings button clicked");
-        });
+//             console.log("Ratings button clicked");
+//         });
 
-//  EventListener 
 
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+    var searchInput = document.getElementById('searchInput');
+
+    document.getElementById('searchButton').addEventListener('click', function () {
+      var searchTerm = searchInput.value.trim();
+
+      if (searchTerm !== '') {
+        searchHistory.push(searchTerm);
+        localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+        console.log('Search History:', searchHistory);
+
+        
+        searchInput.value = '';
+      }
+    });
+  });
 
 
