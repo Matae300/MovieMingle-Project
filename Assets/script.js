eda08ecae8ccf17a3950a46fe61d7eccbc6ba57f
Matae300 <150396856+Matae300@users.noreply.github.com>
2024-01-11 19:18:07 -0500

    added js and changed wheel

--- Assets/script.js
+++ Assets/script.js
@@ -5,3 +5,9 @@
 /* Api calls */
 
 
+var img = document.getElementById("wheel");
+img.addEventListener("click", function(){
+  if(img.src != "Images/R.gif"){
+    img.src = "https://picsum.photos/400/400";}
+  else{
+    img.src = "Images/R.gif";}});
